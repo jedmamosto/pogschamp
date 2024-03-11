c6604adbb39484b1a5414aaca3cb8a802e0ca692
jedmamosto <jedmamosto@gmail.com>
2024-03-11 12:50:48 +0800

    feat: CRUD

---
+++ src/app/api/pogs/[id]/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+  try {
+    const id = params.id;
+
+    const pog = await prisma.pogs.findUnique({
+      where: { id: Number(id) },
+    });
+
+    return NextResponse.json(pog);
+  } catch (error) {
+    console.error('Error fetching pog:', error);
+    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+  }
+}
